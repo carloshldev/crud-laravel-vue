91fb896fefb0ad104cfb8fa5641bf8abc726689e
unknown <cosmikcarlos@gmail.com>
2021-03-16 06:52:01 -0600

    changes

---
+++ resources/js/components/Hola.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text">  
+   <div class="text2"> </div>
+  </div>
+</template>
+
+<script>
+     export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
